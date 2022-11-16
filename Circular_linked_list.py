252281a381b2d76bf9ddc72621c3119189234276
Khan <khan.hoang@outlook.com>
2022-11-16 20:26:52 +0700

    Create Circular_linked_list.py

---
+++ Circular_linked_list.py
@@ -0,0 +1,143 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def prepend(self, data):
+        new_node = Node(data)
+        cur = self.head
+        new_node.next = self.head
+        if not self.head:
+            new_node.next = new_node
+        else:
+            while cur.next != self.head:
+                cur = cur.next
+            cur.next = new_node
+        self.head = new_node
+    
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            self.head.next = self.head
+        else:
+            new_node = Node(data)
+            cur = self.head
+            while cur.next != self.head:
+                cur = cur.next
+            cur.next = new_node
+            new_node.next = self.head
+    
+    def print_list(self):
+        cur = self.head
+
+        while cur:
+            print(cur.data)
+            cur = cur.next
+            if cur == self.head:
+                break
+
+    def remove(self, key):
+        if self.head:
+            if self.head.data == key:
+                cur = self.head
+                while cur.next != self.head:
+                    cur = cur.next
+                if self.head == self.head.next:
+                    self.head = None
+                else:
+                    cur.next = self.head.next
+                    self.head = self.head.next
+            else:
+                cur = self.head
+                prev = None
+                while cur.next != self.head:
+                    prev = cur
+                    cur = cur.next
+                    if cur.data == key:
+                        prev.next = cur.next
+                        cur = cur.next
+
+    def __len__(self):
+        cur = self.head
+        count = 0
+        while cur:
+            count += 1
+            cur = cur.next
+            if cur == self.head:
+                break
+        return count
+    
+    def split_list(self):
+        size = len(self)
+
+        if size == 0:
+            return None
+        if size == 1:
+            return self.head
+
+        mid = size // 2
+        count = 0
+
+        prev = None
+        cur = self.head
+
+        while cur and count < mid:
+            count += 1
+            prev = cur
+            cur = cur.next
+        prev.next = self.head
+
+        split_cllist = CircularLinkedList()
+        while cur.next != self.head:
+            split_cllist.append(cur.data)
+            cur = cur.next
+        split_cllist.append(cur.data)
+
+        self.print_list()
+        print("\n")
+        split_cllist.print_list()
+
+    def josephus_circle(self, step):
+        cur = self.head
+
+        while len(self) > 1:
+            count = 1
+            while count != step:
+                cur = cur.next
+                count += 1
+            print("Removed: ", cur.next.data)
+            self.remove(cur.next.data)
+            cur = cur.next
+
+
+llist = CircularLinkedList()
+llist.append("C")
+llist.append("D")
+llist.prepend("B")
+llist.prepend("A")
+llist.append("E")
+llist.print_list()
+
+print("Remove B")
+llist.remove("B")
+llist.print_list()
+
+print("Length: ", len(llist))
+
+print("Splitting")
+llist.split_list()
+
+print("Josephus Circle")
+llist1 = CircularLinkedList()
+llist1.append("A")
+llist1.append("B")
+llist1.append("C")
+llist1.append("D")
+llist1.append("E")
+llist1.print_list()
+llist1.josephus_circle(1)
+llist1.print_list()
